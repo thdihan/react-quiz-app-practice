c028fc3d341f1857af2f1f4e97a449553c6055e1
thdihan <thdihan@outlook.com>
2023-08-01 09:30:07 +0600

    fix all

--- src/firebase.js
+++ src/firebase.js
@@ -9,6 +9,12 @@ const app = initializeApp({
     storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_REACT_APP_APP_ID,
+//       apiKey: "AIzaSyBM4TDowE5OmJsnQ8IwJhWpseOvZjAO01Y",
+//   authDomain: "react-quiz-dev-bf801.firebaseapp.com",
+//   projectId: "react-quiz-dev-bf801",
+//   storageBucket: "react-quiz-dev-bf801.appspot.com",
+//   messagingSenderId: "140552950187",
+//   appId: "1:140552950187:web:0e4b370eab11af40cd02ee"
 });
 
 export default app;
